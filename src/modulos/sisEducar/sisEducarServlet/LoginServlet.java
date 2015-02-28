d0d0d8badbb750315038b1b478ea9dfb5d80e653
JoãoPaulo <tccsiseducar@gmail.com>
2015-02-27 21:44:53 -0300

    Conserto de Css do login

--- src/modulos/sisEducar/sisEducarServlet/LoginServlet.java
+++ src/modulos/sisEducar/sisEducarServlet/LoginServlet.java
@@ -2,7 +2,6 @@ package modulos.sisEducar.sisEducarServlet;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -44,17 +43,13 @@ public class LoginServlet extends SisEducarServlet
 			Boolean resultado = false;
 			SisEducarServlet sisEducarServlet = new SisEducarServlet();
 			
-			if(usuario.getNome()!=null && usuario.getNome().length() == 0 && usuario.getSenha()!=null && usuario.getSenha().length() == 0)
+			if(usuario.getNome()!=null && usuario.getNome().length() == 0)
 			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Os campos usuário/senha são obrigatórios", null));  
-			}
-			else if(usuario.getNome()!=null && usuario.getNome().length() == 0)
-			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "O campo usuário é obrigatório", null));  
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Usuário é obrigatório", null));  
 			}
 			else if(usuario.getSenha()!=null && usuario.getSenha().length() == 0)
 			{
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "O campo senha é obrigatório", null));  
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Senha é obrigatório", null));  
 			}
 			
 			if(usuario!=null && usuario.getNome()!=null && usuario.getSenha()!=null)
@@ -79,27 +74,14 @@ public class LoginServlet extends SisEducarServlet
 		} 
 		catch (Exception e) 
 		{
-			Logs.addError("Validar login", "Erro ao validar o login, contate o administrador.");
+			Logs.addFatal("Validar login", "Erro ao validar o login, contate o administrador.");
 		}
 	}
 	
-	/**
-	 * Método usado para deslogar um usuário, perdendo a sessão do mesmo
-	 */
-	public void encerrarSessao()
+	public void teste()
 	{
-		try
-		{
-			//FacesContext fc = FacesContext.getCurrentInstance();  
-		    //HttpSession session = (HttpSession)fc.getExternalContext().getSession(false);  
-		    //session.invalidate(); 
-
-			FacesContext.getCurrentInstance().getExternalContext().redirect(ConstantesSisEducar.PATH_PROJETO_NOME + "modulos/sisEducar/mainSisEducar.jsf");
-		}
-		catch (Exception e)
-		{
-			Logs.addError("Encerrar sessão", "Erro ao encerrar a sessão, contate o administrador.");
-		}
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "asasasa", null));  
 	}
 	
 	/**
@@ -120,41 +102,6 @@ public class LoginServlet extends SisEducarServlet
 		{
 			return null;
 		}      
-	}
-	
-	/**
-	 * O método é usado para verificar o horario atual e transmitir uma mensagem de boas vindas ao usuário de acordo com o horario
-	 * @param date
-	 * @return
-	 */
-	public void validaPeriodoHorario(Date date)
-	{
-		try 
-		{
-			@SuppressWarnings("deprecation")
-			Integer hora = date.getHours();
-			
-			if(hora >=0 && hora <=5)
-			{
-				nomeUsuarioLogado = "Boa noite: " + usuario.getNome();
-			}
-			else if(hora >=6 && hora <=11)
-			{
-				nomeUsuarioLogado = "Bom dia: " + usuario.getNome();
-			}
-			else if(hora >=12 && hora <=17)
-			{
-				nomeUsuarioLogado = "Boa tarde: " + usuario.getNome();
-			}
-			else if(hora >=18)
-			{
-				nomeUsuarioLogado = "Boa noite: " + usuario.getNome();
-			}
-		} 
-		catch (Exception e) 
-		{
-			Logs.addError("Validar período", "Erro ao validar período do dia, contate o administrador.");
-		}
 	}
 	
 	/* Getters and Setters */
