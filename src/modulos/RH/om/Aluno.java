b482f2e9eae410edddb200d5fe81f87b139c6844
mwserafima <mwserafima@STFJGRC028730-W.stefanini.dom>
2015-12-21 17:34:18 -0200

    testes ultimamenmente

--- src/modulos/RH/om/Aluno.java
+++ src/modulos/RH/om/Aluno.java
@@ -5,6 +5,7 @@ public class Aluno
 	private int pkAluno;
 	private String ra;
 	private Pessoa pessoa;
+	private int anoLetivo;
 
 	public String getRa() {
 		return ra;
@@ -23,5 +24,11 @@ public class Aluno
 	}
 	public void setPkAluno(int pkAluno) {
 		this.pkAluno = pkAluno;
+	}
+	public int getAnoLetivo() {
+		return anoLetivo;
+	}
+	public void setAnoLetivo(int anoLetivo) {
+		this.anoLetivo = anoLetivo;
 	}
 }
