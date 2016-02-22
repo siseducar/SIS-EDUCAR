60109ab57c06aabb1556bab34f33d2dac0801b5f
joaopaulo <joaopaulo@mresolucoes.com.br>
2016-02-22 20:22:11 -0300

    Inserido novos campos nas OMs

--- src/modulos/RH/om/UnidadeEscolar.java
+++ src/modulos/RH/om/UnidadeEscolar.java
@@ -6,9 +6,51 @@ public class UnidadeEscolar
 	private String codigo;
 	private String nome;
 	private int status;
+	private Boolean unidadeControlada;
+	private Boolean unidadeInformatizada;
 	private RedeEnsino redeEnsino;
 	private Endereco endereco;
+	private TipoOcupacao tipoOcupacao;
+	private SituacaoFuncionamento situacaoFuncionamento;
+	private Terreno terreno;
+	private Regiao regiao;
 	
+	public Boolean getUnidadeControlada() {
+		return unidadeControlada;
+	}
+	public void setUnidadeControlada(Boolean unidadeControlada) {
+		this.unidadeControlada = unidadeControlada;
+	}
+	public Boolean getUnidadeInformatizada() {
+		return unidadeInformatizada;
+	}
+	public void setUnidadeInformatizada(Boolean unidadeInformatizada) {
+		this.unidadeInformatizada = unidadeInformatizada;
+	}
+	public TipoOcupacao getTipoOcupacao() {
+		return tipoOcupacao;
+	}
+	public void setTipoOcupacao(TipoOcupacao tipoOcupacao) {
+		this.tipoOcupacao = tipoOcupacao;
+	}
+	public SituacaoFuncionamento getSituacaoFuncionamento() {
+		return situacaoFuncionamento;
+	}
+	public void setSituacaoFuncionamento(SituacaoFuncionamento situacaoFuncionamento) {
+		this.situacaoFuncionamento = situacaoFuncionamento;
+	}
+	public Terreno getTerreno() {
+		return terreno;
+	}
+	public void setTerreno(Terreno terreno) {
+		this.terreno = terreno;
+	}
+	public Regiao getRegiao() {
+		return regiao;
+	}
+	public void setRegiao(Regiao regiao) {
+		this.regiao = regiao;
+	}
 	public int getPkUnidadeEscolar() {
 		return pkUnidadeEscolar;
 	}
