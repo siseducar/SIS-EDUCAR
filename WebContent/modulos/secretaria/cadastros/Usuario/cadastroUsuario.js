063ad9a15d791d2c9fbc83fd43189bbad694cc7d
João Paulo <tccsiseducar@gmail.com>
2016-03-31 23:09:43 -0300

    Acertado o visual do cadastro de usuário e adicionado as máscaras nessa tela

--- WebContent/modulos/secretaria/cadastros/Usuario/cadastroUsuario.js
+++ WebContent/modulos/secretaria/cadastros/Usuario/cadastroUsuario.js
@@ -1,3 +1,17 @@
+$(function($) {
+	/*	Componente CPF  */
+	$(".cpf").on('focus',function(){
+		$(this).css({"background-color":"#FFFAFA","color":"#000000"});
+		$(this).attr('placeholder','Ex.: 000.000.000-00');
+        $(this).mask("999.999.999-99");
+    });
+	$(".cpf").blur(function(){
+		$(this).css({"background-color":"#FFFFFF","color":"#000000"});
+		$(this).attr('placeholder','');
+        $(this).unmask();
+    });
+});
+
 /**
  * Método usado para atualizar o campo field escondido na tela com o gênero selecionado na tela de usuário
  * @author João Paulo
